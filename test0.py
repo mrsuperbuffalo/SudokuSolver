71278fd7fb9132b8eb42bc863e96758fe9460e07
Richard Lewis <rlewis@decisionvis.com>
2016-10-22 03:37:44 -0400

    double checking get block

--- test0.py
+++ test0.py
@@ -8,7 +8,9 @@ def main(args):
     puzzle = SudokuPuzzle()
     puzzle.read_puzzle(args.input)
     # pprint(puzzle.values)
-    pprint(puzzle.get_block(6))
+    print(puzzle.get_block_rows(1))
+    print(puzzle.get_block_columns(1))
+    pprint(puzzle.get_block(1))
 
 def cli():
     """Command line interface for search algorithm."""
